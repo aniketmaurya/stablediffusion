8a04ec4b245d1c1f77ee286c71762e7c439c77f2
Ethan Harris <ethanwharris@gmail.com>
2022-11-25 23:47:34 +0000

    Fixes

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open(_PATH_REQUIRE) as fp:
 
 setup(
     name='stable-diffusion',
-    version='0.0.6',
+    version='0.0.7',
     description='',
     packages=find_packages(),
     install_requires=requirements,
