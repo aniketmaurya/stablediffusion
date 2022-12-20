6857ce845f18b8d3929197bd28409b62e43a9bab
thomas <thomas@thomass-MacBook-Pro.local>
2022-12-20 13:42:08 +0100

    remove progress bar

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open(_PATH_REQUIRE) as fp:
 
 setup(
     name='stable-diffusion',
-    version='0.0.9',
+    version='0.0.10',
     description='',
     packages=find_packages(),
     install_requires=requirements,
