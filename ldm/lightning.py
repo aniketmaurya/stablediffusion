938199f6fe182489a4dfa91ceeae5c3738f62ef2
Your Name <you@example.com>
2022-12-21 16:11:30 -0500

    update

--- ldm/lightning.py
+++ ldm/lightning.py
@@ -1,13 +1,18 @@
 from typing import List
 from ldm.util import instantiate_from_config
 from ldm.models.diffusion.ddim import DDIMSampler
-
+import base64
 from omegaconf import OmegaConf
 import lightning as L
 import torch
+from io import BytesIO
 from torch.utils.data import Dataset
 import numpy as np
 from PIL import Image
+from einops import rearrange, repeat
+from io import BytesIO
+from contextlib import nullcontext
+from torch import autocast
 
 
 class PromptDataset(Dataset):
@@ -73,4 +78,91 @@ class LightningStableDiffusion(L.LightningModule):
 
             x_samples_ddim = (255.0 * x_samples_ddim).astype(np.uint8)
             pil_results = [Image.fromarray(x_sample) for x_sample in x_samples_ddim]
+        return pil_results
+
+
+class LightningStableImg2ImgDiffusion(L.LightningModule):
+    def __init__(
+        self,
+        config_path: str,
+        checkpoint_path: str,
+        device: str,
+        size: int = 512,
+    ):
+        super().__init__()
+
+        config = OmegaConf.load(config_path)
+        pl_sd = torch.load(checkpoint_path, map_location="cpu")
+        if "global_step" in pl_sd:
+            print(f"Global Step: {pl_sd['global_step']}")
+        sd = pl_sd["state_dict"]
+        self.model = instantiate_from_config(config.model).to(device)
+        self.model.load_state_dict(sd, strict=False)
+
+        self.to(device)
+
+        self.sampler = DDIMSampler(self.model)
+
+        self.initial_size = int(size / 8)
+        self.steps = 50
+
+        self._device = device
+
+    def serialize_image(self, image: str):
+        init_image = base64.b64decode(image)
+        buffer = BytesIO(init_image)
+        init_image = Image.open(buffer, mode="r").convert("RGB")
+        w, h = init_image.size
+        w, h = map(lambda x: x - x % 32, (w, h))  # resize to integer multiple of 32
+        image = init_image.resize((w, h), resample=Image.LANCZOS)
+        image = np.array(image).astype(np.float32) / 255.0
+        image = image[None].transpose(0, 3, 1, 2)
+        image = torch.from_numpy(image)
+        return 2. * image - 1.
+
+    @torch.no_grad()
+    def predict_step(
+        self,
+        inputs: str,
+        batch_idx: int,
+        batch_size: int = 1,
+        precision=16,
+        strength=0.75, 
+        scale = 5.0
+    ):
+        prompt, init_image = inputs
+
+        init_image = self.serialize_image(init_image).to(self._device)
+        init_image = repeat(init_image, '1 ... -> b ...', b=batch_size)
+        init_latent = self.model.get_first_stage_encoding(self.model.encode_first_stage(init_image))
+
+        self.sampler.make_schedule(ddim_num_steps=self.steps, ddim_eta=0.0, verbose=False)
+
+        t_enc = int(strength * self.steps)
+
+        precision_scope = autocast if precision == 16 else nullcontext
+        with precision_scope("cuda"):
+            with self.model.ema_scope():
+                uc = None
+                if scale != 1.0:
+                    uc = self.model.get_learned_conditioning(batch_size * [""])
+                c = self.model.get_learned_conditioning(prompt)
+
+                # encode (scaled latent)
+                z_enc = self.sampler.stochastic_encode(init_latent, torch.tensor([t_enc]*batch_size).to(self._device))
+                # decode it
+                samples = self.sampler.decode(
+                    z_enc,
+                    c,
+                    t_enc,
+                    unconditional_guidance_scale=scale,
+                    unconditional_conditioning=uc,
+                )
+
+                x_samples_ddim = self.model.decode_first_stage(samples)
+                x_samples_ddim = torch.clamp((x_samples_ddim + 1.0) / 2.0, min=0.0, max=1.0)
+                x_samples_ddim = x_samples_ddim.cpu().permute(0, 2, 3, 1).numpy()
+
+                x_samples_ddim = (255.0 * x_samples_ddim).astype(np.uint8)
+                pil_results = [Image.fromarray(x_sample) for x_sample in x_samples_ddim]
         return pil_results
---
+++ scripts/img2img_lightning.py
@@ -0,0 +1,207 @@
+"""make variations of input image"""
+
+import argparse, os
+import PIL
+import base64
+import torch
+import numpy as np
+from io import BytesIO
+from omegaconf import OmegaConf
+from PIL import Image
+from tqdm import tqdm, trange
+from itertools import islice
+from einops import rearrange, repeat
+from torchvision.utils import make_grid
+from torch import autocast
+from contextlib import nullcontext
+from pytorch_lightning import seed_everything
+
+from ldm.util import instantiate_from_config
+from ldm.models.diffusion.ddim import DDIMSampler
+from ldm.lightning import LightningStableImg2ImgDiffusion
+
+
+def chunk(it, size):
+    it = iter(it)
+    return iter(lambda: tuple(islice(it, size)), ())
+
+
+def load_model_from_config(config, ckpt, verbose=False):
+    print(f"Loading model from {ckpt}")
+    pl_sd = torch.load(ckpt, map_location="cpu")
+    if "global_step" in pl_sd:
+        print(f"Global Step: {pl_sd['global_step']}")
+    sd = pl_sd["state_dict"]
+    model = instantiate_from_config(config.model)
+    m, u = model.load_state_dict(sd, strict=False)
+    if len(m) > 0 and verbose:
+        print("missing keys:")
+        print(m)
+    if len(u) > 0 and verbose:
+        print("unexpected keys:")
+        print(u)
+
+    model.cuda()
+    model.eval()
+    return model
+
+
+def load_img(path):
+    image = Image.open(path).convert("RGB")
+    buffer = BytesIO()
+    image.save(buffer, format="PNG")
+    buffer.seek(0)
+    encoded = buffer.getvalue()
+    return base64.b64encode(encoded).decode("ascii")
+
+# wget https://pl-public-data.s3.amazonaws.com/dream_stable_diffusion/v1-5-pruned-emaonly.ckpt
+# Command example: python scripts/img2img_lightning.py --prompt "A fantasy landscape, trending on artstation" --init-img assets/stable-samples/img2img/sketch-mountains-input.jpg --strength 0.8 --ckpt ../stable-diffusion/v1-5-pruned-emaonly.ckpt --outdir results --n_samples 1 --config=configs/stable-diffusion/v1-inference.yaml
+def main():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument(
+        "--prompt",
+        type=str,
+        nargs="?",
+        default="a painting of a virus monster playing guitar",
+        help="the prompt to render"
+    )
+
+    parser.add_argument(
+        "--init-img",
+        type=str,
+        nargs="?",
+        help="path to the input image"
+    )
+
+    parser.add_argument(
+        "--outdir",
+        type=str,
+        nargs="?",
+        help="dir to write results to",
+        default="outputs/img2img-samples"
+    )
+
+    parser.add_argument(
+        "--ddim_steps",
+        type=int,
+        default=50,
+        help="number of ddim sampling steps",
+    )
+
+    parser.add_argument(
+        "--fixed_code",
+        action='store_true',
+        help="if enabled, uses the same starting code across all samples ",
+    )
+
+    parser.add_argument(
+        "--ddim_eta",
+        type=float,
+        default=0.0,
+        help="ddim eta (eta=0.0 corresponds to deterministic sampling",
+    )
+    parser.add_argument(
+        "--n_iter",
+        type=int,
+        default=1,
+        help="sample this often",
+    )
+
+    parser.add_argument(
+        "--C",
+        type=int,
+        default=4,
+        help="latent channels",
+    )
+    parser.add_argument(
+        "--f",
+        type=int,
+        default=8,
+        help="downsampling factor, most often 8 or 16",
+    )
+
+    parser.add_argument(
+        "--n_samples",
+        type=int,
+        default=2,
+        help="how many samples to produce for each given prompt. A.k.a batch size",
+    )
+
+    parser.add_argument(
+        "--n_rows",
+        type=int,
+        default=0,
+        help="rows in the grid (default: n_samples)",
+    )
+
+    parser.add_argument(
+        "--scale",
+        type=float,
+        default=9.0,
+        help="unconditional guidance scale: eps = eps(x, empty) + scale * (eps(x, cond) - eps(x, empty))",
+    )
+
+    parser.add_argument(
+        "--strength",
+        type=float,
+        default=0.8,
+        help="strength for noising/unnoising. 1.0 corresponds to full destruction of information in init image",
+    )
+
+    parser.add_argument(
+        "--from-file",
+        type=str,
+        help="if specified, load prompts from this file",
+    )
+    parser.add_argument(
+        "--config",
+        type=str,
+        default="configs/stable-diffusion/v2-inference.yaml",
+        help="path to config which constructs model",
+    )
+    parser.add_argument(
+        "--ckpt",
+        type=str,
+        help="path to checkpoint of model",
+    )
+    parser.add_argument(
+        "--seed",
+        type=int,
+        default=42,
+        help="the seed (for reproducible sampling)",
+    )
+    parser.add_argument(
+        "--precision",
+        type=str,
+        help="evaluate at this precision",
+        choices=["full", "autocast"],
+        default="autocast"
+    )
+
+    opt = parser.parse_args()
+    seed_everything(opt.seed)
+
+    model = LightningStableImg2ImgDiffusion(
+        config_path=opt.config,
+        checkpoint_path=opt.ckpt,
+        device="cuda" if torch.cuda.is_available() else "cpu",
+    )
+
+    os.makedirs(opt.outdir, exist_ok=True)
+    outpath = opt.outdir
+
+    init_image = load_img(opt.init_img)
+
+    images = model.predict_step((opt.prompt, init_image), 0)
+
+    os.makedirs(outpath, exist_ok=True)
+    grid_count = len(os.listdir(outpath)) - 1
+
+    for image in images:
+        image.save(os.path.join(outpath, f'grid-{grid_count:04}.png'))
+        grid_count += 1
+
+
+if __name__ == "__main__":
+    main()
